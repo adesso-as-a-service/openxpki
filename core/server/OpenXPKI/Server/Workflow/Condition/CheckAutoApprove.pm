37ac575ca1adca7082ec3079a44337d2991073cc
kariustobias <tobias.karius@yahoo.de>
2022-02-10 13:38:50 +0100

    fixed short-dns bug

--- core/server/OpenXPKI/Server/Workflow/Condition/CheckAutoApprove.pm
+++ core/server/OpenXPKI/Server/Workflow/Condition/CheckAutoApprove.pm
@@ -18,7 +18,6 @@ sub _evaluate
     my $ser = OpenXPKI::Serialization::Simple->new;
 
     my $white_list = $self->param->{whitelist};
-    my $dns_entries = $self->param->{dns_entries};
 
     CTX('log')->application()->info("white_list param: $white_list \n");
     # similar to OpenXPKI::Server::Workflow::Activity::CSR::CheckPolicyDNS
@@ -42,19 +41,10 @@ sub _evaluate
 
     my $print_sans = join(", ", @cnsans);
     CTX('log')->application()->info("Testing if White-List matches with $print_sans");
-    
-    #parse dns_entries
-    my @dns_entries_array = split /:/, $dns_entries;
 
-    # initialize dns resolver
-    my $resolver = Net::DNS::Resolver->new;
-    my $timeout = $self->param('timeout') || 30;
-    $resolver->udp_timeout( $timeout );
-    $resolver->tcp_timeout( $timeout );
-    $resolver->retry(1);
 
     foreach my $value (@cnsans) {
-        my $res = validateCNSANs($value, $white_list, $resolver, \@dns_entries_array);
+        my $res = validateCNSANs($value, $white_list, \@cnsans);
         if ($res) {
             CTX('log')->application()->error("$value does not match with regex $white_list : $res Cannot auto-approve");
             condition_error('cn or san does not match with regex. Cannot auto-approve');
@@ -65,31 +55,38 @@ sub _evaluate
 }
 
 # checks, if cn or san is in white-list.
-# also checks for short-dns entries and performs a dns lookup
-# returns 1, if cn or san not in white-list. If cn or san is a short-dns: returns 1, if not in white-list and/or dns lookup fails
-# returns 0, if cn or san in white-list. If cn or san is a short-dns: returns 0 if in white-list and dns lookup succeeds.
+# also checks for short-dns entries.
+# returns 1, if cn or san not in white-list. If cn or san is a short-dns: returns 1, if no matching fqdn available.
+# returns 0, if cn or san in white-list. If cn or san is a short-dns: returns 0 if in white-list and fqdn available.
 
 sub validateCNSANs {
-    my ($value_validate, $white_list_validate, $dns_resolver, $ref_dns_entries_array) = @_;
+    my ($value_validate, $white_list_validate, $ref_cnsans) = @_;
 
     if ($value_validate !~ m{$white_list_validate}) {
         CTX('log')->application()->info("Testing for short-dns names.");
         my $reply;
         # check for short-dns
-        foreach my $dns_entry (@{$ref_dns_entries_array}){
-            my $fqdn = $value_validate.".".$dns_entry;   
-            if ($fqdn =~ m{$white_list_validate}) {
-                eval { $reply = $dns_resolver->send( $fqdn ); };
-                if ($reply && $reply->answer) {
-                    CTX('log')->application()->info("Short dns-name $value_validate validated by white-list entry $dns_entry and resolved dns. Auto-Approve successful.");
+        if ($value_validate !~ m{^\.}) {
+            FQDN:
+            foreach my $value_ref_cnsans (@{$ref_cnsans}) {
+                if ($value_ref_cnsans !~ m{^\.}) {
+                    next FQDN;
+                }
+
+                if ($value_validate eq (split /\.\, $value_ref_cnsans)[0]) {
+                    # we can return 0 without checking the fqdn, since auto-approval would then fail at fqdn
                     return 0;
                 }
             }
+            # short-dns, but no fqdn
+            return 1;
         }
-        # no dns resolve or/and white-list matching
-        return 1;
+        else{
+            # no short-dns and no white-list match
+            return 1;
+        }
     }
-    # no short dns and matches
+    # fqdn matches whitelist
     return 0;
 }
 
